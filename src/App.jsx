99ca8f3160b469ce777d198132540d5d9bc6e233
unknown <Chakib13000>
2025-01-31 20:43:53 +0100

    Ajout des fichiers du projet

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Project/project';
+import Skills from './sections/Skills/skills';
+
+function App() {
+  return (
+    <>
+   <Hero/>
+   <Projects/>
+   <Skills/>
+   <Contact/>
+   <Footer/>
+   </>)
+}
+
+export default App
---
+++ src/sections/Project/project.jsx
@@ -0,0 +1,42 @@
+import fitLift from '../../assets/fitlift.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import viberr from '../../assets/viberr.png';
+import ProjectCard from '../../common/ProjectCard';
+import styles from './Projectstyles.module.css';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Viberr"
+          p="Streaming App"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Fresh Burger"
+          p="Hamburger Restaurant"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hipsster"
+          p="Glasses Shop"
+        />
+        <ProjectCard
+          src={fitLift}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="FitLift"
+          p="Fitness App"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
